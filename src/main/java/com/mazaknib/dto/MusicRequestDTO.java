3e3b6064f14d2ee5e3fb38cec0a469302dcfd68a
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-01-27 16:02:19 +0100

    added valiation to the dtos

--- src/main/java/com/mazaknib/dto/MusicRequestDTO.java
+++ src/main/java/com/mazaknib/dto/MusicRequestDTO.java
@@ -1,14 +1,22 @@
 package com.mazaknib.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class MusicRequestDTO {
+    @NotBlank
+    @NotEmpty(message = "name should not be empty ")
     private String name;
+    @NotEmpty(message = "description should not be empty ")
     private String description;
 
     // Input files from the form-data
+    @NotNull(message = "image should not be empty ")
     private MultipartFile imageFile;
+    @NotNull(message = "audio should not be empty ")
     private MultipartFile audioFile;
 }
