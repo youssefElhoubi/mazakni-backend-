b13e3167ceafa73ace90ba1c76f957c3c976169d
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-01-27 14:04:54 +0100

    added annotations to the repository and the serviec

--- src/main/java/com/mazaknib/repository/MusicRepository.java
+++ src/main/java/com/mazaknib/repository/MusicRepository.java
@@ -2,6 +2,8 @@ package com.mazaknib.repository;
 
 import com.mazaknib.entity.Music;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MusicRepository extends MongoRepository<Music, String> {
 }
--- src/main/java/com/mazaknib/service/MusicService.java
+++ src/main/java/com/mazaknib/service/MusicService.java
@@ -1,4 +1,9 @@
 package com.mazaknib.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MusicService{
 }
