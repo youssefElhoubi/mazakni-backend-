94fd890a661e9dc21acb136445bdd9fd5818dc4d
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-01-27 17:23:05 +0100

    made methods for making od the audio upload

--- src/main/java/com/mazaknib/service/MusicService.java
+++ src/main/java/com/mazaknib/service/MusicService.java
@@ -1,9 +1,50 @@
 package com.mazaknib.service;
 
+import com.mazaknib.dto.MusicRequestDTO;
+import com.mazaknib.dto.MusicResponseDTO;
+import com.mazaknib.entity.Music;
+import com.mazaknib.mapper.MusicMapper;
+import com.mazaknib.repository.MusicRepository;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.gridfs.GridFsOperations;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class MusicService{
+    private final MusicRepository musicRepository;
+    private final MusicMapper musicMapper;
+    private final GridFsTemplate gridFsTemplate;
+    private final GridFsOperations operations;
+
+    public MusicResponseDTO save(MusicRequestDTO req) throws IOException {
+        Music music = musicMapper.toEntity(req);
+
+        String musicImage = upload(req.getImageFile());
+        String musicAudio = upload(req.getAudioFile());
+
+        music.setImage(musicImage);
+        music.setAudio(musicAudio);
+
+        return musicMapper.toResponse(musicRepository.save(music));
+    }
+
+    private String upload(MultipartFile file) throws IOException {
+        DBObject object = new BasicDBObject();
+        object.put("type", file.getContentType());
+        ObjectId id = gridFsTemplate.store(
+                file.getInputStream(),
+                file.getOriginalFilename(),
+                file.getContentType(),
+                object
+        );
+        return id.toString();
+    }
 }
