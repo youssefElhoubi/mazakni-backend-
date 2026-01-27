639344a0bfdb47ad80fb3db3585fc28a355270a9
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-01-27 16:04:14 +0100

    complate the music mapper

--- src/main/java/com/mazaknib/mapper/MusicMapper.java
+++ src/main/java/com/mazaknib/mapper/MusicMapper.java
@@ -1,4 +1,22 @@
 package com.mazaknib.mapper;
 
+import com.mazaknib.dto.MusicRequestDTO;
+import com.mazaknib.dto.MusicResponseDTO;
+import com.mazaknib.entity.Music;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface MusicMapper {
+
+    // We ignore file fields here; the Service will handle the upload and setting of IDs
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "image", ignore = true)
+    @Mapping(target = "audio", ignore = true)
+    Music toEntity(MusicRequestDTO request);
+
+    // Map the entity's stored String IDs to the Response DTO
+    @Mapping(source = "image", target = "imageId")
+    @Mapping(source = "audio", target = "audioId")
+    MusicResponseDTO toResponse(Music music);
 }
